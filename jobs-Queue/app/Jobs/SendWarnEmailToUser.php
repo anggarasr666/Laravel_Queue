c2c7e07e668ac7d5230073856803069c27bdda72
Angga <angga060803@gmail.com>
2023-11-08 10:12:40 +0700

    Jobs-Queue

---
+++ jobs-Queue/app/Jobs/SendWarnEmailToUser.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendWarnEmailToUser implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function __invoke(): void
+    {
+        sleep(3);
+        info("Hello 1");
+        sleep(2);
+        info("Hello 2");
+        sleep(2);
+        info("it'll show after 7 seconds");
+    }
+}
